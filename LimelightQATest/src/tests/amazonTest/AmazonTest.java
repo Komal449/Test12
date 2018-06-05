27b6420459a85690b26a0cec549b896f1cf39063
Komal patel <patelbkomal@gmail.com>
2018-06-05 16:56:23 -0700

    no message

--- LimelightQATest/src/tests/amazonTest/AmazonTest.java
+++ LimelightQATest/src/tests/amazonTest/AmazonTest.java
@@ -46,7 +46,7 @@ public class AmazonTest {
 		srp.sortinglistPrice(firstFiveItems);
 		srp.sortinglistRating(firstFiveItems);
 		int x = 1,y= 3, r;
-		r= x+y;
+		r= x+4;
 		system.out.println(r);
 	
 		
